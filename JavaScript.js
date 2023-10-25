dd835ad98ae8f98264bf076b9bb6915b021a8b0c
Cristiano Ferro De Franca <135919856+CristianoFranca1976@users.noreply.github.com>
2023-10-25 17:48:35 +0100

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,17 @@
-function myFunction() {
-alert("Eu sei que nao e o certo mas foi o melhor que consegui");
+function myMove() {
+  let id = null;
+  const elem = document.getElementById("bom");
+  let pos = 5;
+  clearInterval(id);
+  id = setInterval(frame, 1);
+  function frame() {
+    if (pos == 641) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.marginLeft = "595px";
+      elem.style.right = pos + "-950px";
+      elem.style.left = pos + "px";
+    }
+  }
 }
